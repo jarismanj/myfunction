35f7da6abb1672735b8cb03887cfb4ed8f4ae6ef
jarisman <jarisman@gmail.com>
2017-02-14 12:58:01 +0800

    first r

---
+++ function.r
@@ -0,0 +1,20 @@
+add2 <- function(x,y) {
+  x+y
+}
+
+above10 <- function(x) {
+  use <- x > 10
+  x[use]
+}
+
+x <- 1:20
+above(x)
+
+columnmean <- function(y {
+  mc <- ncol(y)
+  means <- numeric(nc)
+  for(i in 1:nc) {
+    means[i]<means(y,[l]<mean(y, [, i]))
+  }
+  means }
+}
